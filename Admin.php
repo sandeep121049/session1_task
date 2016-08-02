5a87476e4bfbc0ace1c17ab4b6d0c28592106774
rajyasree123 <rajyasree.delgence@gmail.com>
2016-08-02 17:49:39 +0530

    updatedhjghjgkkkkkrrrrr

---
+++ Admin.php
@@ -0,0 +1,31 @@
+<html>
+	<head>
+	</head>	
+	<body>
+		<form action="Page2.php" method="POST" name="frm2">
+		<table>
+		<tr>
+				<td>Link</td>
+				<td><input type="text" name="t2"></td>
+			</tr>
+			<tr>
+				<td>MetaDesc</td>
+				<td><textarea name="t3"></textarea></td>
+			</tr>
+			<tr>
+				<td>Title</td>
+				<td><input type="text" name="t1"></td>
+			</tr>
+			
+			
+			<tr>
+				<td>MetaLongDesc</td>
+				<td><textarea name="t4"></textarea></td>
+			</tr>
+			<tr>
+				<td align="center"><input type="submit" name="submit" value="Submit"></td>
+			</tr>
+		</table>
+		</form>
+	</body>
+<html>
